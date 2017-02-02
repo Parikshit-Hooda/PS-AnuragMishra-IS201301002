a30f95ad774f392c3d5e95f09d95b2b86fd6d029
itsanurag94 <mishraanurag565@gmail.com>
2017-02-03 00:53:23 +0530

    Updated sample tasks (assignment 2) and weekly learning

---
+++ Assignments/Assignment 2/Int_To_Binary.r
@@ -0,0 +1,14 @@
+n<- readline(prompt="Please enter a Positive number to convert it into binary: ")
+n<-as.integer(n)
+ans<-c()
+tmp<-n
+for(i in 1:n)
+{
+  ans[i]<-as.integer(tmp%%2)
+  tmp<-tmp/2
+  if(tmp<=0)
+  {
+    break
+  }
+}
+print(rev(ans))
